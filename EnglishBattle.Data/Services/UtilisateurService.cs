d4d5eb2196d65fab83862064c08abe728b890c0e
Ludho <Ludho234@gmail.com>
2022-02-09 21:42:18 +0100

    Initialize

---
+++ EnglishBattle.Data/Services/UtilisateurService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishBattle.Data.Services
+{
+    public class UtilisateurService
+    {
+        private EnglishBattleEntities context;
+
+        public UtilisateurService(EnglishBattleEntities context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Retourne un objet métier
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>objet métier</returns>
+        public Joueur GetItem(int id)
+        {
+            using (context)
+            {
+                return context.Joueur.Find(id);
+            }
+        }
+
+        public Joueur GetItem(string email, string password)
+        {
+            using (context)
+            {
+                IQueryable<Joueur> utilisateurs = from Joueur in context.Joueur
+                                                       where Joueur.email == email
+                                                       && Joueur.motDePasse == password
+                                                       select Joueur;
+
+                return utilisateurs.FirstOrDefault();
+            }
+        }
+
+        public List<Joueur> GetList()
+        {
+            using (context)
+            {
+                return context.Joueur.ToList();
+
+            }
+        }
+
+        public void Insert(Joueur Joueur)
+        {
+            using (context)
+            {
+                context.Joueur.Add(Joueur);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Joueur Joueur)
+        {
+            using (context)
+            {
+                context.Entry(Joueur).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Joueur Joueur)
+        {
+            using (context)
+            {
+                context.Entry(Joueur).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EnglishBattleData/Security/sa.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [sa]
+    WITH PASSWORD = N'mfxauhnylpiwxq.dC|iibmq>msFT7_&#$!~<h=qTovrpQa4P', DEFAULT_LANGUAGE = [us_english];
+
